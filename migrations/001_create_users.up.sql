9f5dd332b962dc7c2a602f56ec54ff4f47fc3dfa
Sanjaee <afrizaahmad18@gmail.com>
2025-11-15 12:01:09 +0700

    Remove uniqueness constraint from username field in users table schema and migration files.

--- migrations/001_create_users.up.sql
+++ migrations/001_create_users.up.sql
@@ -4,7 +4,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS users (
     userId VARCHAR(36) PRIMARY KEY DEFAULT uuid_generate_v4()::text,
     googleId VARCHAR(255) UNIQUE,
-    username VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255),
     profilePic TEXT,
